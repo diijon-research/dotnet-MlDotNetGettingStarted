655561d4e0e25efd71c4f14317c22507cf7b39a3
Diijon <diijon@gmail.com>
2018-08-26 15:50:26 +0000

    set application path and data path

--- src/SnailAge/Program.cs
+++ src/SnailAge/Program.cs
@@ -1,21 +1,25 @@
 ﻿using System;
+using System.IO;
 using Microsoft.ML;
 using Microsoft.ML.Data;
 using Microsoft.ML.Models;
 using Microsoft.ML.Transforms;
 using Microsoft.ML.Trainers;
 using System.Threading.Tasks;
-
+using System.Reflection;
 
 namespace MlDotNetGettingStarted.SnailAge
 {
     class Program
     {
-        private static string _datapath;
+        private static string _applicationPath = "";
+        private static string _datapath = "";
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _applicationPath = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().Location).LocalPath);
+            _datapath = $"{_applicationPath}/data/abalone.data.txt";
+            Console.WriteLine(_datapath);
         }
 
         public static async Task<PredictionModel<Abalone, AbaloneAgePrediction>> Train()
